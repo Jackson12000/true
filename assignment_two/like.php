1750fbfc5a9a48ce070796e20e493f744cb58e8d
Aimeedian <160475314+Aimeedian@users.noreply.github.com>
2024-04-10 12:53:07 +0200

    Add files via upload

---
+++ assignment_two/like.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost"; 
+$username = "admin"; 
+$password = "bityear2@2024"; 
+$dbname = "bityeartwo2024"; 
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Select the database
+$conn->select_db($dbname);
+
+$sql = "INSERT INTO likes (contentid,userid)
+VALUES ('$_POST[content]', '$_POST[user]')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "data inserted  successfully<br>";
+} else {
+    echo "Error inserting  data: " . $conn->error;
+}
+
+// Close connection
+$conn->close();
+?>
---
+++ assignment_two/login.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+$servername = "localhost"; 
+$username = "admin"; 
+$password = "bityear2@2024"; 
+$dbname = "bityeartwo2024"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        $_SESSION['username'] = $username;
+        header("Location: home page.html"); 
+        exit();
+    } else {
+        $error = "Invalid username or password";
+    }
+}
+
+$conn->close();
+?>
